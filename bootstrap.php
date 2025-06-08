2f263e08b8dd063022d5b336f7f45cbb331c5a2a
Fabian Carvajal <inbox@edgcarmu.me>
2025-06-08 14:24:07 -0600

    Update error reporting environment cases in bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -66,11 +66,12 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
+	case 'dnamic':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
 	break;
 
-	case 'testing':
+	case 'staging':
 	case 'production':
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
